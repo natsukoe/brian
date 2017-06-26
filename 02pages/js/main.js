78737565042fa47770c4ac82bc03461f13a99f0e
Natsuko <natsuko_eguchi@yahoo.com>
2017-06-26 09:13:18 -0700

    video slide moved

--- 02pages/js/main.js
+++ 02pages/js/main.js
@@ -54,6 +54,13 @@ var q_3_span = document.querySelector('#q-3 span');
 var q_4_span = document.querySelector('#q-4 span');
 
 var slides = [
+
+	{
+		slideVideo: 'business1/firstMilkyWay.mp4',
+		templateType: 'video'
+	},
+
+
 	{
 		slideTitle: 'Introduction',
 		slideText: '<p>Welcome to Carus Group\'s, Peru Illinois headquarters.</p><p>The employees with Carus are committed to maintaining a safe work environment. At our headquarters, safety is a condition of employment. We ask that you always look for safety hazards, be aware of your environment and be your brother\'s keeper.</p><p>SAFETY IS FIRST - NO EXCEPTIONS!!!</p>',
@@ -63,10 +70,7 @@ var slides = [
 		templateType: 'basic'
 	},
 
-	{
-		slideVideo: 'business1/firstMilkyWay.mp4',
-		templateType: 'video'
-	},
+
 
 	{
 		slideTitle: 'Introduction',
