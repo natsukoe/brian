450783353d156369c8ad70e498e07331eb37937f
Natsuko <natsuko_eguchi@yahoo.com>
2017-09-08 16:07:38 -0700

    updated how question pagenation displays

--- 02pages/js/main090817.js
+++ 02pages/js/main090817.js
@@ -2,7 +2,7 @@
 
 var course_title = "MasterBrand Cabinets, Inc. (MBCI)";
 var sub_title = "Jasper, Indiana";
-var header_color = "#b1273e"; // #e3dd1b #b1273e
+var header_color = "#e3dd1b"; // #e3dd1b #b1273e
 var header_txt_color = "#fff";
 var user_name = 'Admin';
 var user_status = 'admin';
@@ -14,6 +14,11 @@ document.getElementsByTagName('h2')[0].innerHTML = sub_title;
 // Applying a custom header and text color
 $("#masterhead").css({"background":header_color});
 $("#masterhead").css({"color":header_txt_color});
+
+//$( auto_play_input.checked + " .slider").css({"background-color":header_color});
+//$("#auto-play-input:focus").css({"box-shadow":header_color});
+//$(".state-label-on").css({"color":header_txt_color});
+
 
 // If user is admin add special features and CSS style
 if ( user_status === 'admin' ) {
@@ -306,8 +311,56 @@ var question_slides = [
 		referenceSlide: 22
 	}
 	// Before course slide 23
-];
 
+];
+/*
+var remediation_slides = [
+	// After course slide 11
+	{
+		slideTitle: 'Contractor Responsibilities',
+		slideText: '<p><font color="#FF0000">While on site at any MBCI location, contractors must comply with all applicable national, federal, state, regional and local health and safety regulations and codes, as well as MBCI company policies.</font></p><p>MBCI company policies are available upon request.</p>',
+		slideAudio: 'MasterBrand/11.mp3',
+		slideMedia: 'MasterBrand/11.jpg',
+		slideMediaAlt: 'Image of SlideMedia for page 5',
+		templateType: 'basic',
+		slideType: 'remediation'
+	},
+	// Before course slide 12
+	// After course slide 12
+	{
+		slideTitle: 'Contractor Responsibilities',
+		slideText: '<p><font color="#FF0000">Contractors are responsible for providing their employees with all training related to applicable national, federal, state, regional and local health and safety regulations and codes that apply to their activities and the products and services they provide to MBCI.</font>  MBCI may request copies of contractor training documentation.</p><p>Contractors are responsible for having all required licenses, permits and certifications necessary to fulfill the contract in compliance with all applicable laws and regulations.</p>',
+		slideAudio: 'MasterBrand/12.mp3',
+		slideMedia: 'MasterBrand/12.jpg',
+		slideMediaAlt: 'Image of SlideMedia for page 5',
+		templateType: 'basic',
+		slideType: 'remediation'
+	},
+	// Before course slide 13
+	// After course slide 21
+	{
+		slideTitle: 'Personal Conduct',
+		slideText: '<p>Other Personal Conduct rules include:</p><ul><li><font color="#FF0000">Horseplay: reckless conduct and behavior is strictly prohibited</font></li><li><font color="#FF0000">Weapons: the possession of any weapon in violation of state or local regulations is prohibited on MBCI property, and</font></li><li><font color="#FF0000">Smoking: smoking is not allowed, except in designated smoking areas; such areas will be communicated during pre-job reviews</font></li></ul></p><p>Non-compliance with MBCI personal conduct rules is a serious offense and can lead to dismissal from the worksite.</p>',
+		slideAudio: 'MasterBrand/21.mp3',
+		slideMedia: 'MasterBrand/21.jpg',
+		slideMediaAlt: 'Image of SlideMedia for page 5',
+		templateType: 'basic',
+		slideType: 'remediation'
+	},
+	// Before course slide 22
+	// After course slide 22
+	{
+		slideTitle: 'Plant Rules and Information',
+		slideText: '<p><font color="#86381A"><b>Driving and Parking</b></font></p><p><font color="#FF0000">Contractors are to park in designated areas only, and not in MBCI employee parking areas unless authorized by MBCI site EHS personnel.</p><p>All contractor employees driving a vehicle on MBCI property must have a valid driver\'s license.</p><p>Unless otherwise posted, the maximum speed limit in MBCI parking lots is <b>5 mph</b>; the maximum speed limit inside any part of an MBCI building is <b>3 mph</b>.</font></p>',
+		slideAudio: 'MasterBrand/22.mp3',
+		slideMedia: 'MasterBrand/22.jpg',
+		slideMediaAlt: 'Image of SlideMedia for page 5',
+		templateType: 'basic',
+		slideType: 'remediation'
+	}
+	// Before course slide 23
+];
+*/
 var slides_mapper = {
 	/*11: [1],
 	12: [2],
@@ -357,10 +410,17 @@ document.getElementById('jump-button').addEventListener("click", function(){
 	if ( jump_page.value === "" || jump_page.value === null ) {
 		return;
 	}
+
+	if ( slides_mapper.hasOwnProperty( current_slide_num ) ) {
+		//bookmarked = jump_page.value + 1;
+	} else {
+		//bookmarked = jump_page.value;
+	}
 	bookmarked = jump_page.value;
 	current_slide_num = jump_page.value - 1;
 	slides_work();
 	pg_current.innerHTML = current_slide_num + 1;
+	//bookmarked = jump_page.value;
 });
 
 // Counting the total number of question slides
@@ -398,6 +458,14 @@ function prev_slide() {
 		if ( current_slide_num > 0 ) {
 		current_slide_num--;
 		pg_current.innerHTML = current_slide_num + 1;
+		/*
+		if ( slides[ current_slide_num ].templateType !== 'question' || slides[ current_slide_num ].slideType !== 'remediation' ) {
+			current_course_num--;
+			
+		}
+		while ( slides[ current_slide_num ].templateType === 'question' || slides[ current_slide_num ].slideType === 'remediation' ) {
+			current_slide_num--;
+		}*/	
 	}
 }
 
@@ -418,6 +486,8 @@ function next_slide() {
 				current_slide_num = resume_question_num;
 				question_array--;
 				slide_text.removeAttribute('class', 'remediation');
+				//questions_load();
+				//console.log("question_array: " + question_array);
 			} else {
 				current_slide_num++;
 				current_course_num++;
@@ -439,15 +509,37 @@ function next_slide() {
 		}
 	}	
 }
+/*
+function skip_next_slide() {
+	console.log("current_slide_num" + current_slide_num);
+	current_slide_num = current_slide_num + 1;
+	bookmarked = bookmarked + 1;
+	console.log("current_slide_num" + current_slide_num);
+}
+*/
 
 // Bookmarking
 // New bookmarking
 function writeBookmark() {
+	/*if (bookmarked - 1 >=  current_slide_num) {
+		console.log("bookmarked already");
+	}
+	else if*/ 
 	if ( bookmarked === 0 || bookmarked - 1 <  current_slide_num ) {
 		bookmarked++;
 		//updateProgress();
 	}
 }
+
+/*
+function writeBookmark() {
+	if ( bookmarked === 0 || bookmarked <= current_slide_num || !slides_mapper.hasOwnProperty( current_slide_num ) ) {
+		bookmarked++;
+		console.log("Now bookmark updated to : " + bookmarked);
+		//updateProgress();
+	}
+}
+*/
 
 // Using only after remediation slide
 function decreaseBookmark() {
@@ -460,7 +552,8 @@ auto_play_input.addEventListener("click", function() {
 	if ( auto_play_input.checked ) {
 		document.getElementById('auto-play').setAttribute("class", "on-state");
 
-		// When audio is not playing and auto run is set ON, move slide to next
+		// When audio is not playing and auto run is set ON, move slide to next	
+		// media_audio.duration < 0
 		if ( media_audio.paused && media_audio.currentTime === media_audio.duration ) {
 			next_slide();
 			slides_work();
@@ -531,6 +624,8 @@ function course_load() {
 
 		var media_img = document.createElement('img');
 		media_img.setAttribute('src', slides[ current_slide_num ].slideMedia);
+		//media_img.setAttribute("width", "304");
+		//media_img.setAttribute("width", "228");
 		media_img.setAttribute('alt', slides[ current_slide_num ].slideMediaAlt);
 		body_media.appendChild(media_img);
 
@@ -628,6 +723,7 @@ function questions_load() {
 	tempplate_basic.setAttribute("class", "display-no"); 
 	template_video.removeAttribute("class", "display-yes");
 	template_question.setAttribute("class", "display-yes block-padding");
+	//media_video.src = slides[ slides_num ].slideVideo;
 
 	// Clearing previously selected answer
 	var quizInput = document.querySelectorAll("input[name='tfQuestion'], input[name='4choices']");
@@ -645,6 +741,7 @@ function questions_load() {
 	document.getElementById('question-wrong').setAttribute("class", "display-no");
 
 	// Getting an appropriate question form
+
 	if ( typeof ( question_slides[ question_index ].quizChoice ) == 'undefined' ) {
 		// Load the True or False form	
 		document.getElementById('question-tf').setAttribute("class", "display-yes"); 
@@ -676,14 +773,52 @@ function questions_load() {
 			q_1_input.value = randomNum[0];
 			q_2_input.value = randomNum[1];
 			q_3_input.value = randomNum[2];
+			//q_4_input.value = randomNum[3];
 
 			q_1_span.innerHTML = question_slides[ question_index ].quizChoice[ randomNum[0] - 1 ];
 			q_2_span.innerHTML = question_slides[ question_index ].quizChoice[ randomNum[1] - 1 ];
 			q_3_span.innerHTML = question_slides[ question_index ].quizChoice[ randomNum[2] - 1 ];
+			//q_4_span.innerHTML = question_slides[ question_index ].quizChoice[ randomNum[3] - 1 ];
+
 		}
 
 		// Getting Question
 		question_q.innerHTML = question_slides[ question_index ].quizQuestion;
+			
+			/* The below was dvelopping the Select All that Apply type of answers. JS needs to be tweak a bit more when answer slots shuffle to make it work  */
+			// Getting all selected values from quiz multi
+			/*
+			var selectedMulti = [];
+
+			document.getElementById('multi-submit').addEventListener("click", function(){
+				$("input:checkbox[type=checkbox]:checked").each(function(){
+		    		selectedMulti.push($(this).val());
+				});	
+
+
+			if ( selectedMulti.length == slides[ slides_num ].quizAnswer.length ) {
+
+				var a;
+				var i;
+
+				for ( a = 0; a < slides[ slides_num ].quizAnswer.length; a++ ) {
+					for ( i = 0; i < slides[ slides_num ].quizAnswer.length; i++ ) {
+						if ( slides[ slides_num ].quizAnswer[a] == selectedMulti[i] ) {
+							// let's go next page
+							console.log("You've gotten it!");
+						}
+						else {
+							console.log("Wrong answer..");
+						}
+					}
+				}
+			}
+			else {
+				console.log( "Wrong num of answer!" );
+			}
+
+			});
+			*/
 
 		// Hiding Pagenation and Showing the Embeded Txt in HTML
 		document.getElementById('pagenation').setAttribute("class", "display-no");
@@ -693,8 +828,103 @@ function questions_load() {
 		document.getElementById('q-current').innerHTML = question_index + 1;
 		document.getElementById('q-total').innerHTML = question_slides.length;
 
+	} else {
+		//tempplate_basic.removeAttribute("class", "display-no");
+		//template_video.removeAttribute("class", "display-yes");
 	}
 }
+/*
+function remediation_load() {
+	
+	// This block is to accommodate next button particularly on video slide gets disabled initially on iPhone
+	if ( slides[ current_slide_num ].templateType === 'basic' || 'video' || 'remediation' ) {
+		// disable next btn when no bookmarked pg or current slide num is smaller than bookmarked
+		if ( current_slide_num >= bookmarked ) {
+			document.getElementById('pg-next').setAttribute('disabled', true);
+		}
+	}
+
+		// Getting text
+		document.getElementsByTagName('h3')[0].innerHTML = remediation_slides[ question_index ].slideTitle;
+		slide_text.innerHTML = remediation_slides[ question_index ].slideText;
+
+		var body_media = document.getElementById('bodymedia');
+		var media_default = body_media.childNodes[ question_index ];
+
+		// Removing image from previous page
+	    while (body_media.hasChildNodes()) {
+	        body_media.removeChild(body_media.firstChild);
+	    }
+
+		// Loading Basic Template
+		if ( remediation_slides[ question_index ].templateType === 'basic' ) {
+			// Choosing a proper template
+			template_video.removeAttribute("class", "display-yes");
+			template_question.removeAttribute("class", "display-yes");
+
+			// Show the page controller
+			document.getElementById('pg-buttons').removeAttribute("class", "display-no");
+
+			var media_img = document.createElement('img');
+			media_img.setAttribute('src', remediation_slides[ question_index ].slideMedia);
+			//media_img.setAttribute("width", "304");
+			//media_img.setAttribute("width", "228");
+			media_img.setAttribute('alt', remediation_slides[ question_index ].slideMediaAlt);
+			body_media.appendChild(media_img);
+
+			// Inserting an audio file
+			media_audio.src = remediation_slides[ question_index ].slideAudio;
+
+			// Hiding Pagenation and Showing the Embeded Txt in HTML
+			if ( remediation_slides[ question_index ].slideType === 'remediation' ) {
+				document.getElementById('pagenation').setAttribute("class", "display-no");
+				document.getElementById('pg-remediation').setAttribute("class", "display-yes");
+				document.getElementById('pg-question').removeAttribute("class", "display-yes");
+			}
+			else {
+				document.getElementById('pagenation').removeAttribute("class", "display-no");
+				document.getElementById('pg-question').removeAttribute("class", "display-yes");
+				document.getElementById('pg-remediation').removeAttribute("class", "display-yes");
+			}
+		}
+
+		// Loading Video Template
+		if ( remediation_slides[ question_index ].templateType === 'video' ) {
+			// Removing audio that has run previously
+			media_audio.pause();
+			// Choosing a proper template	
+			tempplate_basic.setAttribute("class", "display-no"); 
+			template_video.setAttribute("class", "display-yes block-padding");
+			template_question.setAttribute("class", "display-no");   
+			media_video.src = remediation_slides[ question_index ].slideVideo;
+
+			// Show the page controller
+			document.getElementById('pg-buttons').removeAttribute("class", "display-no");
+
+			// Hiding Pagenation and Showing the Embeded Txt in HTML
+			document.getElementById('pagenation').removeAttribute("class", "display-no");
+			document.getElementById('pg-question').removeAttribute("class", "display-yes");
+		}
+		else {
+			tempplate_basic.removeAttribute("class", "display-no");
+			template_video.removeAttribute("class", "display-yes");
+		}
+		
+		// Removing video that has run previously just in case
+		if ( remediation_slides[ question_index ].templateType != 'video' ) {
+			media_video.pause();
+		}
+
+		// Disable back button when current page is 1 and remediation slids
+		if ( current_slide_num == 0 || remediation_slides[ question_index ].slideType === 'remediation' ) {
+			document.getElementById('pg-back').setAttribute("disabled", true);
+		}
+		// Enable back button when current page is NOT 1
+		else {
+			document.getElementById('pg-back').removeAttribute('disabled');
+		}
+}
+*/
 
 // Checking if the submitted answer is correct or wrong
 document.getElementById('question-submit').addEventListener("click", function(){
@@ -730,6 +960,7 @@ document.getElementById('question-submit').addEventListener("click", function(){
 		setTimeout(function(){
 			current_slide_num = question_slides[ question_index ].referenceSlide - 1;
 			pg_current.innerHTML = current_slide_num + 1;
+			//decreaseBookmark();
 			course_load();
 			slide_text.setAttribute("class", "remediation");
 			document.getElementById('pg-back').setAttribute("disabled", true);
@@ -791,6 +1022,16 @@ $(media_audio).on('playing', function() {
 		document.getElementById('pg-next').removeAttribute('disabled');
 		replay = 0;
 	}
+
+	/*
+	// disable next btn when no bookmarked pg or current slide num is smaller than bookmarked
+	if ( bookmarked === 0 || current_slide_num >= bookmarked ) {
+		document.getElementById('pg-next').setAttribute('disabled', true);
+	}
+	else {
+		document.getElementById('pg-next').removeAttribute('disabled');
+	}
+	*/
 });
 
 $(media_audio).on('ended', function() {
@@ -800,7 +1041,8 @@ $(media_audio).on('ended', function() {
 
    // enable button
    document.getElementById('pg-next').removeAttribute('disabled');
-   // Use this if bookmarking at the end of narration is preferred	
+   // Use this if bookmarking at the end of narration is preferred
+   //writeBookmark(); 	
 
 	// auto play on
 	if ( auto_play_input.checked && current_slide_num + 1 < slides.length ) {
@@ -843,6 +1085,7 @@ $(media_video).on('ended', function() {
    // enable button
    document.getElementById('pg-next').removeAttribute('disabled');
    // New bookmarking
+   //writeBookmark();
 
 	// auto play on
 	if ( auto_play_input.checked ) {
