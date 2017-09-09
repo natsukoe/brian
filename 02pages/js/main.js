47f8e789dc157ba005acbd3ddf593bc16b225653
Natsuko <natsuko_eguchi@yahoo.com>
2017-09-08 18:16:43 -0700

    updating JS

--- 02pages/js/main.js
+++ 02pages/js/main.js
@@ -860,6 +860,20 @@ $(media_video).on('pause', function() {
 });
 
 
+
+    $(function() {      
+      //Enable swiping...
+      $("#auto-play-input").swipe( {
+        //Generic swipe handler for all directions
+        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+          console.log("You swiped " + direction );  
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+         threshold:0
+      });
+    });
+
+
 // Sowing progress bar
 /*
 function updateProgress() {
